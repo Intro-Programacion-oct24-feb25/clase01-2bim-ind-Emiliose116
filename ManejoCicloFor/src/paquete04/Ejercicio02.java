fc989ebc07cea66e15c89eedc784beb9b8628a12
Emilio J Cueva <ecueva611@gmail.com>
2024-12-05 12:10:07 -0500

    Cambio Ejercicio2

--- ManejoCicloFor/src/paquete04/Ejercicio02.java
+++ ManejoCicloFor/src/paquete04/Ejercicio02.java
@@ -9,5 +9,30 @@ package paquete04;
  * @author utpl
  */
 public class Ejercicio02 {
-    
+
+    public static void main(String[] args) {
+
+        double celcius;
+        double fahrenheit;
+        int contador = 0;
+        String cadena = "";
+
+        for (fahrenheit = 20; fahrenheit <= 100; fahrenheit += 4) {
+
+            celcius = (5.0 / 9.0) * (fahrenheit - 32);
+
+            cadena = String.format("%sOperacion(%d)Fahrenheit: %.2f ===> "
+                    + "Celcius: %.2f\n", 
+                    cadena, 
+                    contador, 
+                    fahrenheit, 
+                    celcius);
+
+            contador = contador + 1;
+
+        }
+
+        System.out.printf("%s", cadena);
+
+    }
 }
